911cf4b81c481ca4fbe246ec959b04900e0857d7
Takayuki Wada <takafoot@hotmail.fr>
2023-10-23 08:21:26 +0200

    created the route to request token and the middleware to authenticate with the Token

--- server.js
+++ server.js
@@ -11,3 +11,41 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Node API app is running on port ${PORT}`);
 });
+
+// request Token
+
+app.post("/api/token", async (req, res) => {
+  const userEmail = req.headers.email;
+  const email = { email: userEmail };
+
+  const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+
+  if (!(await redis.get(userEmail[0]))) {
+    await redis.hset(userEmail, {
+      count: 0,
+      token: accessToken,
+      email: userEmail,
+    });
+  }
+});
+
+// Middleware authenticateToken
+
+const authenticateToken = async (req, res, next) => {
+  /*const authHeader = req.headers["authorization"];
+  const token = authHeader;
+  if (!token) return res.sendStatus(401);*/
+
+  const { email } = req.body;
+  const verifyToken = await redis.hget(email, "token", function (err, obj) {
+    return obj;
+  });
+
+  jwt.verify(verifyToken, process.env.ACCESS_TOKEN_SECRET, (err, email) => {
+    if (err) return res.sendStatus(403);
+    console.log(email, "this is the email");
+    req.email = email;
+    next();
+  });
+};
