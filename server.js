1138085e350c4534c903fd2d5c36cbf33b8f1e06
Takayuki Wada <takafoot@hotmail.fr>
2023-10-23 09:24:34 +0200

    created the middleware to check the rate limit of a user

--- server.js
+++ server.js
@@ -13,34 +13,6 @@ app.listen(PORT, () => {
   console.log(`Node API app is running on port ${PORT}`);
 });
 
-// request Token
-
-app.post("/api/token", async (req, res) => {
-  const userEmail = req.headers.email;
-  const email = { email: userEmail };
-
-  const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken: accessToken });
-
-  if (!(await redis.get(userEmail[0]))) {
-    await redis.hset(userEmail, {
-      count: 0,
-      token: accessToken,
-      email: userEmail,
-    });
-  }
-});
-
-// request justify api
-
-app.post("/api/justify", authenticateToken, (req, res) => {
-  const { text, email } = req.body;
-  res.type("txt");
-
-  let response = justifyText(text, 80, email);
-  res.send(response);
-});
-
 // Middleware authenticateToken
 
 const authenticateToken = async (req, res, next) => {
@@ -61,9 +33,56 @@ const authenticateToken = async (req, res, next) => {
   });
 };
 
+// Middleware rate limit
+
+const rateLimitChecking = async (req, res, next) => {
+  const { email } = req.body;
+  const rateLimit = 500;
+
+  const userCount = await redis.hget(email, "count");
+
+  if (userCount > rateLimit) {
+    res.status(402).send("Payment Required");
+  } else {
+    next();
+  }
+};
+
+// request Token
+
+app.post("/api/token", async (req, res) => {
+  const userEmail = req.body.email;
+  const email = { email: userEmail };
+
+  const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+  res.status(200).json({ email: userEmail, accessToken: accessToken });
+
+  try {
+    const response = await redis.hset(userEmail, {
+      count: 0,
+      token: accessToken,
+      email: userEmail,
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error, "this is an error");
+  }
+});
+
+// request justify api
+
+app.post("/api/justify", authenticateToken, rateLimitChecking, (req, res) => {
+  const { text, email } = req.body;
+  res.type("txt");
+
+  let response = justifyText(text, 80, email);
+  res.status(200).send(response);
+});
+
 // Justify function
 
-const justifyText = async (text, maxLength, email) => {
+const justifyText = (text, maxLength, email) => {
   const words = text.split(" ");
   let currentLine = "";
   const lines = [];
@@ -86,7 +105,7 @@ const justifyText = async (text, maxLength, email) => {
     let justifyLine = "";
     for (let j = 0; j < AmountOfWordInline.length; j++) {
       let word = AmountOfWordInline[j];
-      console.log(word);
+      //console.log(word);
       if (numberOfCharacterInLine > 0) {
         justifyLine += word + "  ";
         numberOfCharacterInLine -= 1;
@@ -98,6 +117,6 @@ const justifyText = async (text, maxLength, email) => {
     }
     lines[i] = justifyLine;
   }
-  //tawait redis.hset(email, "count", words.length);
+  redis.hincrby(email, "count", words.length);
   return lines.join("\n");
 };
