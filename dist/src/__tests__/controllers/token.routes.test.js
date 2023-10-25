7655f8b4319dcf2d2e45a41e091d90c76dc35fc1
Takayuki Wada <takafoot@hotmail.fr>
2023-10-25 09:33:41 +0200

    rebuilt it

--- dist/src/__tests__/controllers/token.routes.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-it("is a silly test", () => {
-    expect("hello").toBe("hello");
-    //googleSearch("this is a test", dbMock);
-});
-//# sourceMappingURL=token.routes.test.js.map
--- dist/src/__tests__/routes/justify.routes.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-it("is a silly test", () => {
-    expect("hello").toBe("hello");
-    //googleSearch("this is a test", dbMock);
-});
-//# sourceMappingURL=justify.routes.test.js.map
